c6f55dbf3db2a112f69afd46f5cfec3f6d45556c
hilbert-spaess <jacobcoxon@hotmail.co.uk>
2021-11-16 21:21:16 +0000

    comma separated

--- allocate.py
+++ allocate.py
@@ -7,14 +7,14 @@ def allocate(values):
     # normalise the values into pounds
 
     normed_values = values * (3000000/np.sum(values))
-    normed_values = [f"£{x:.2f}" for x in normed_values]
+    normed_values = [f"£{round(x, -3):,.2f}" for x in normed_values]
 
     # produce an allocation
     
     projects = pd.read_csv("variables.csv",
                            usecols=['Number', 'Name'])
     projects['Allocation'] = normed_values
-
+    projects = projects.sort_values(['Allocation'])
     projects.to_csv("allocation.csv", index=False)
 
 allocate(values)
